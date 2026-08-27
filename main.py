f6942523433da11191ec7594cd5e68bb3a93cecf
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2026-08-27 13:58:53 -0700

    upd1

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 #main python code file
+
+import time 
+
 kai = "K.A.I."
 
 print("I'm K.A.I., your personal A.I. assistant! ")
+
+time.sleep(1.5)
+
+
 print("")
 print("Who am I working with today? ")
 
-usrName = input(": ")
+usrName = input(":")
 
 print()
 print("Welcome " + usrName + ". What can I do for you? ")
 
+frstCmd = input(":")
 
