480a80e354c27eb912576d59c3dfa111007f9043
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2026-08-28 13:50:35 -0700

    upd1.2 web service

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 import time 
 import json
 import os
+import webbrowser
+import urllib.parse
+from urllib.parse import quote
 
 
 kai = "K.A.I."
@@ -42,7 +45,7 @@ if usrName != "Kai":
 
     if adult:
         print("Welcome " + usrName + ". What can I help you with? ")
-        frstCmd = input(": ")
+        frstCmd = input(":")
     if not adult:
         print()
         print("I'm sorry, I am restricted from minors.")
@@ -53,8 +56,102 @@ if frstCmd == "nothing":
     time.sleep(1)
     print("Let me know when I can help.")
 
+    frstCmd = input(":")
+
+
 if frstCmd == "math":
+    time.sleep(0.5)
+
     print()
     print("Whats our equation? ")
-    mathQ = int(input(""))
-    
+    mathQ = input(":")
+    print()
+
+    print("Calculating..")
+    time.sleep(0.5)
+    mathA = eval(mathQ)
+    print(mathA)
+
+if frstCmd != "math" and frstCmd != "nothing":
+    query = frstCmd
+    encoded_query = urllib.parse.quote(query)
+
+    url = f"https://www.google.com/search?q={encoded_query}"
+
+    print()
+    print(f"Searching browser for: {query}")
+    webbrowser.open(url)
+
+
+print()
+print("Anything else? ")
+sndCmd = input(":")
+
+if sndCmd == "nothing":
+    print()
+    time.sleep(1)
+    print("Let me know when I can help.")
+
+    sndCmd = input(":")
+
+
+if sndCmd == "math":
+    time.sleep(0.5)
+    print()
+    print("Whats our equation? ")
+    mathQ = input(":")
+    print()
+
+    print("Calculating..")
+    time.sleep(0.5)
+    mathA = eval(mathQ)
+    print(mathA)
+
+if sndCmd != "math" and sndCmd != "nothing":
+    query = sndCmd
+    encoded_query = urllib.parse.quote(query)
+
+    url = f"https://www.google.com/search?q={encoded_query}"
+
+    print()
+    print(f"Searching browser for: {query}")
+    webbrowser.open(url)
+
+time.sleep(1)
+
+print()
+print("Anything else? ")
+trdCmd = input(":")
+
+if trdCmd == "nothing":
+    print()
+    time.sleep(1)
+    print("Let me know when I can help.")
+
+    trdCmd = input(":")
+
+
+if trdCmd == "math":
+    time.sleep(0.5)
+    print()
+    print("Whats our equation? ")
+    mathQ = input(":")
+    print()
+
+    print("Calculating..")
+    time.sleep(0.5)
+    mathA = eval(mathQ)
+    print(mathA)
+
+if trdCmd != "math" and sndCmd != "nothing":
+    query = trdCmd
+    encoded_query = urllib.parse.quote(query)
+
+    url = f"https://www.google.com/search?q={encoded_query}"
+
+    print()
+    print(f"Searching browser for: {query}")
+    webbrowser.open(url)
+
+time.sleep(1)
+print()
